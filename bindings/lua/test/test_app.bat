7da3c77e33c758038d758cfad4d5ad869413997f
LunaCI <lunaci@luadist.org>
2018-06-08 02:48:27 +0000

    Update to version 0.1.0-1

---
+++ bindings/lua/test/test_app.bat
@@ -0,0 +1,43 @@
+@Echo Off
+
+:: Now we declare a scope
+Setlocal EnableDelayedExpansion EnableExtensions
+
+start cmd /c lua testsrv.lua ^> testsrv.log 2^>^&1
+echo --------------------------------------
+echo server started
+echo --------------------------------------
+
+sleep 30
+lua utestcli.lua
+
+set code=%errorlevel%
+
+echo --------------------------------------
+echo Exit %code%
+echo --------------------------------------
+
+taskkill /F /IM lua.exe
+type testsrv.log
+sleep 30
+
+start cmd /c lua testsrv_rs232.lua ^> testsrv.log 2^>^&1
+echo --------------------------------------
+echo server started
+echo --------------------------------------
+
+sleep 30
+lua utestcli_rs232.lua
+
+if "%code%"=="0" (
+	set code=%errorlevel%
+)
+
+echo --------------------------------------
+echo Exit %code%
+echo --------------------------------------
+
+taskkill /F /IM lua.exe
+type testsrv.log
+
+exit /B %code%
